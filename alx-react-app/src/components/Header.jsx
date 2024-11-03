da443d724e578326768554919072630913d82997
onucheojo <mandofman@gmail.com>
2024-11-03 17:36:19 +0100

    components

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(){
     return (
         <div>
             <header>
-                <h1>My Favourite Cities </h1>
+                <h1>My Favorite Cities </h1>
             </header>
         </div>
     );
