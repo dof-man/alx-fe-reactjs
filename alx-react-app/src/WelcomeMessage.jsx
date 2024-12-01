fdfb7253270bdfab584c86455a999030fdccc4f6
onucheojo <mandofman@gmail.com>
2024-12-01 23:02:45 +0100

    tailwindcss

--- alx-react-app/src/WelcomeMessage.jsx
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -1,6 +1,7 @@
+const header = {color: "black",backgroundColor:"gray"}
 function WelcomeMessage() {
     return (
-        <div>
+        <div style={header}>
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX!</p>
