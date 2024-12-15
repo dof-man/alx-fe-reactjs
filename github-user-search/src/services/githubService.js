e48fb4ac0dc74b473f113f88110528b9cd4c925e
onucheojo <mandofman@gmail.com>
2024-12-15 17:47:29 +0100

    search

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/search/users';
+
+export const fetchUserData = async ({ searchTerm, location, minRepos }) => {
+    let query = searchTerm ? `${searchTerm} in:login` : '';
+    if (location) query += ` location:${location}`;
+    if (minRepos) query += ` repos:>${minRepos}`;
+
+    try {
+        const response = await axios.get(`${BASE_URL}?q=${query}`);
+        return response.data.items; // Returns an array of matching users
+    } catch (error) {
+        throw new Error('Failed to fetch user data');
+    }
+};
