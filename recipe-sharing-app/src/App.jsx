234eea8303b0e859c9e939a0b58ed38da23d1138
onucheojo <mandofman@gmail.com>
2025-01-16 00:38:31 +0100

    recipe

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/AddRecipeForm'
-function App() {
-  const [count, setCount] = useState(0)
+import { Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
+const App = () => {
   return (
-    <>
-      
-      
-    </>
-  )
-}
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,21 @@
-import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../components/recipeStore';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <Link to={`/recipe/${recipe.id}`}>
             <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  export default RecipeList
-
-  
+export default RecipeList;
