596ffeda75abcd7cca430f2ced1028380ad7fe36
onucheojo <mandofman@gmail.com>
2025-01-16 01:12:30 +0100

    appbar last

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './FavoritesList';
+import RecommendationsList from './RecommendationsList';
 const App = () => {
   return (
     <div>
