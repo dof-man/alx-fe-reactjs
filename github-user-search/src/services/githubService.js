63be0d951264abd0ea49a2c8c98280da7d526b0d
onucheojo <mandofman@gmail.com>
2025-01-17 02:38:03 +0100

    fix map

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const BASE_URL = 'https://api.github.com/search/users?q';
 
 export const fetchUserData = async ({ searchTerm, location, minRepos }) => {
     let query = searchTerm ? `${searchTerm} in:login` : '';
