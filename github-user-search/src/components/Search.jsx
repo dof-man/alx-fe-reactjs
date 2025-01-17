b64d23f6c503a80e1cce55a4be85ec77d31d6322
onucheojo <mandofman@gmail.com>
2025-01-17 02:23:33 +0100

    fix

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ function Search() {
       const response = await axios.get(`https://api.github.com/users/${username}`);
       setUser(response.data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
